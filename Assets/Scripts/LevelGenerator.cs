4ce5e12acbdc755d27c852fd882e9366be2cccb4
Solacx <junyong6@gmail.com>
2020-09-21 20:06:34 +1000

    Add basic rotation rules to map generator.
    NB: There are some issues in the current rotation rules.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -26,27 +26,135 @@ public class LevelGenerator : MonoBehaviour
             {7, outsideJunction}
         };
 
-        // Get and render map
-        List<List<int>> map = GetMap("Recreation");
+        RenderMap();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /**
+     * Get and render map.
+     *
+     * NB: Rotation uses hardcoded values for map piece type. Also some
+     *     room to improve.
+     */
+    void RenderMap()
+    {
+        int rowIndex = 0;
+        int columnIndex = 0;
+        Vector3 renderAnchor = new Vector3(-7, 7, 0);
         Vector3 renderPos = new Vector3(-7, 7, 0);
+
+        List<List<int>> map = GetMap("Recreation");
         foreach (List<int> row in map)
         {
             foreach (int value in row)
             {
                 if (value != 0)
                 {
-                    Instantiate(mapPieceMapping[value], renderPos, Quaternion.identity);
+                    GameObject mapPiece = Instantiate(mapPieceMapping[value], renderPos, Quaternion.identity);
+                    
+                    // Check rotation
+                    bool doFlipX = false;
+                    bool doFlipY = false;
+                    switch (value)
+                    {
+                        case 1:
+                            if (columnIndex != 0 && map[rowIndex][columnIndex - 1] == 2)
+                            {
+                                doFlipX = true;
+                            }
+                            if (rowIndex != 0 && map[rowIndex - 1][columnIndex] == 2)
+                            {
+                                doFlipY = true;
+                            }
+                            break;
+
+                        case 2:
+                            if (rowIndex != 0 && (map[rowIndex - 1][columnIndex] == 1 || map[rowIndex - 1][columnIndex] == 2))
+                            {
+                                Rotate(mapPiece, 90);
+                            }
+                            break;
+
+                        case 3:
+                            if (columnIndex != 0 && (map[rowIndex][columnIndex - 1] == 3 || map[rowIndex][columnIndex - 1] == 4))
+                            {
+                                doFlipX = true;
+                            }
+                            else if (rowIndex != 0 && (map[rowIndex - 1][columnIndex] == 3 || map[rowIndex - 1][columnIndex] == 4))
+                            {
+                                doFlipY = true;
+                            }
+                            break;
+
+                        case 4:
+                            if (rowIndex != 0 && (map[rowIndex - 1][columnIndex] == 3 || map[rowIndex - 1][columnIndex] == 4
+                                                                                      || map[rowIndex - 1][columnIndex] == 7))
+                            {
+                                Rotate(mapPiece, 90);
+                            }
+                            break;
+                    }
+
+                    // Do rotation
+                    if (doFlipX && doFlipY) {
+                        FlipBoth(mapPiece);
+                    }
+                    else if (doFlipX) { FlipX(mapPiece); }
+                    else if (doFlipY) { FlipY(mapPiece); }
                 }
-                renderPos.x += 1;
+                renderPos.x++;
+                columnIndex++;
             }
-            renderPos.x = -7;
+            renderPos.x = renderAnchor.x;
             renderPos.y -= 1;
+
+            columnIndex = 0;
+            rowIndex++;
         }
     }
 
-    void Update()
+    /**
+     * Flip around the X axis.
+     */
+    void FlipX(GameObject gameObject, SpriteRenderer renderer = null)
     {
-        
+        renderer = (renderer is null) ? gameObject.GetComponent<SpriteRenderer>()
+                                      : renderer;
+        renderer.flipX = true;
+    }
+
+    /**
+     * Flip around the Y axis.
+     */
+    void FlipY(GameObject gameObject, SpriteRenderer renderer = null)
+    {
+        renderer = (renderer is null) ? gameObject.GetComponent<SpriteRenderer>()
+                                      : renderer;
+        renderer.flipY = true;
+    }
+
+    /**
+     * Flip around the X and Y axis.
+     */
+    void FlipBoth(GameObject gameObject)
+    {
+        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+        FlipX(gameObject, renderer);
+        FlipY(gameObject, renderer);
+    }
+
+    /**
+     * Rotate around the apparent X axis.
+     *
+     * For convenience.
+     */
+    void Rotate(GameObject gameObject, float angle)
+    {
+        gameObject.transform.Rotate(0, 0, angle);
     }
 
     /**
